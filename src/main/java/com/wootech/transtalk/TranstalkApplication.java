a0200b8e4473725e22f7d78707043cd46738727f
taeseonYoo <jkl0642@hansung.ac.kr>
2025-11-06 19:07:34 +0900

    feat: Initial commit of Spring Boot project structure

---
+++ src/main/java/com/wootech/transtalk/TranstalkApplication.java
@@ -0,0 +1,13 @@
+package com.wootech.transtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranstalkApplication.class, args);
+	}
+
+}
