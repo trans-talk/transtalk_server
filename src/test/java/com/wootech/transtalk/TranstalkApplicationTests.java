e2551b65090a5323a411b830d5cd0cd6dfd711dd
taeseonYoo <jkl0642@hansung.ac.kr>
2025-11-06 19:07:34 +0900

    feat: Initial commit of Spring Boot project structure

---
+++ src/test/java/com/wootech/transtalk/TranstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wootech.transtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
