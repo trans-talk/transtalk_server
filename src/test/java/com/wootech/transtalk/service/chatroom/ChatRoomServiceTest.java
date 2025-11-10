3ba5b2a722dc5d4aef6133f47ccf59fa41e41676
taeseonYoo <jkl0642@hansung.ac.kr>
2025-11-10 22:33:03 +0900

    test(ChatRoomService): findChatRoomsByUserId integration test
    - write findChatRoomsByUserId test
    - add h2 dependency for test

---
+++ src/test/java/com/wootech/transtalk/service/chatroom/ChatRoomServiceTest.java
@@ -0,0 +1,57 @@
+package com.wootech.transtalk.service.chatroom;
+
+
+import com.wootech.transtalk.dto.ChatMessageRequest;
+import com.wootech.transtalk.dto.chatroom.ChatRoomResponse;
+import com.wootech.transtalk.entity.User;
+import com.wootech.transtalk.enums.UserRole;
+import com.wootech.transtalk.repository.UserRepository;
+import com.wootech.transtalk.service.chat.ChatService;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("Integration - ChatRoomService")
+class ChatRoomServiceTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ChatRoomService chatRoomService;
+    @Autowired
+    private ChatService chatService;
+
+    @Test
+    void findChatRoomsByUserId() {
+        User user = userRepository.save(
+                User.builder().email("tae@google").name("tae").picture("img1").userRole(UserRole.ROLE_USER).build());
+        User recipient = userRepository.save(
+                User.builder().email("other@google").name("other").picture("img2").userRole(UserRole.ROLE_USER)
+                        .build());
+        Long savedChatRoomId = chatRoomService.save("ko", "tae@google", "other@google");
+        chatService.save(new ChatMessageRequest("hello"), savedChatRoomId, "tae@google");
+        List<ChatRoomResponse> responses = chatRoomService.findChatRoomsByUserId(user.getId());
+
+        Assertions.assertThat(responses.get(0))
+                .extracting(
+                        ChatRoomResponse::originalRecentMessage,
+                        ChatRoomResponse::unreadMessageCount,
+                        ChatRoomResponse::chatroomId,
+                        ChatRoomResponse::recipientName,
+                        ChatRoomResponse::recipientPicture,
+                        ChatRoomResponse::selectedLanguage,
+                        ChatRoomResponse::translatedRecentMessage
+                ).contains(
+                        "hello",
+                        1,
+                        savedChatRoomId,
+                        recipient.getName(),
+                        recipient.getPicture(),
+                        "ko",
+                        null
+                );
+    }
+}
