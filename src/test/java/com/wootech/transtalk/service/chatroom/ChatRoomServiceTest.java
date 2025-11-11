533ffb410c322bdaf993d99aa859f0d107bc4fc6
taeseonYoo <jkl0642@hansung.ac.kr>
2025-11-12 00:06:39 +0900

    test(ChatRoomService): integration tests for Chat Room Creation Logic
    - When a request comes in using a new language among the same participants, save
    - If a request is made in the same language between the same participants, return the existing chat room ID

--- src/test/java/com/wootech/transtalk/service/chatroom/ChatRoomServiceTest.java
+++ src/test/java/com/wootech/transtalk/service/chatroom/ChatRoomServiceTest.java
@@ -12,6 +12,7 @@ import com.wootech.transtalk.enums.UserRole;
 import com.wootech.transtalk.service.chat.ChatService;
 import com.wootech.transtalk.service.user.UserService;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,16 +31,24 @@ class ChatRoomServiceTest {
     @Autowired
     private ChatService chatService;
 
+
+    User user;
+    User recipient;
+
+    @BeforeEach
+    void setUp() {
+        user = userService.findByEmailOrGet(
+                "tae@google", "tae", UserRole.ROLE_USER, "img1");
+        recipient = userService.findByEmailOrGet(
+                "other@google", "other", UserRole.ROLE_USER, "img2");
+    }
+
     @Test
     void findChatRoomsByUserId() {
-        User user = userService.findByEmailOrGet(
-                "tae@google", "tae", UserRole.ROLE_USER, "img1");
-        User recipient = userService.findByEmailOrGet(
-                "other@google", "other", UserRole.ROLE_USER, "img2");
         CreateChatRoomResponse response = chatRoomService.save(TranslateLanguage.KOREAN, "tae@google", "other@google");
         chatService.save(new ChatMessageRequest("hello"), response.chatRoomId(), "tae@google");
         Pageable pageable = PageRequest.of(0, 40);
-        Page<ChatRoomResponse> responses = chatRoomService.findChatRoomsByUserId(user.getId(),pageable);
+        Page<ChatRoomResponse> responses = chatRoomService.findChatRoomsByUserId(user.getId(), pageable);
 
         Assertions.assertThat(responses.getContent())
                 .extracting(
@@ -61,5 +70,30 @@ class ChatRoomServiceTest {
                                 (String) null
                         )
                 );
+    }
+
+    @Test
+    @DisplayName("동일한 회원 사이의 동일한 언어로 채팅방 요청이 들어오면, 기존 채팅방 id를 반환한다.")
+    void save_duplicate_request() {
+        //given
+        CreateChatRoomResponse response = chatRoomService.save(TranslateLanguage.KOREAN, "tae@google", "other@google");
+
+        //when
+        CreateChatRoomResponse createChatRoomResponse = chatRoomService.save(TranslateLanguage.KOREAN, "tae@google",
+                "other@google");
+        //then
+        Assertions.assertThat(response.chatRoomId()).isEqualTo(createChatRoomResponse.chatRoomId());
+    }
+    @Test
+    @DisplayName("동일한 회원 사이에 다른 언어로 채팅방 요청이 들어오면, 새로운 채팅방을 생성한다.")
+    void save_other_request() {
+        //given
+        CreateChatRoomResponse response = chatRoomService.save(TranslateLanguage.KOREAN, "tae@google", "other@google");
+
+        //when
+        CreateChatRoomResponse createChatRoomResponse = chatRoomService.save(TranslateLanguage.ENGLISH, "tae@google",
+                "other@google");
+        //then
+        Assertions.assertThat(response.chatRoomId()).isNotEqualTo(createChatRoomResponse.chatRoomId());
     }
 }
